e198985476ae60bc3e383968a16d14aa36082876
Terence Williams <richie2k693@yahoo.com>
2016-10-09 11:46:21 -0400

    Revert "Interact with tracker"
    This reverts commit 9ac92454c53618953cf9c1ba97eff7d4a797aadf.

--- src/GivenTools/RUBTClient.java
+++ src/GivenTools/RUBTClient.java
@@ -3,22 +3,13 @@
  */
 package GivenTools;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.Socket;
 import java.net.URL;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 
 /**
  * @author chai1
@@ -29,16 +20,11 @@ public class RUBTClient {
 	/**
 	 * @param args
 	 */
-	private static int interval;
-	private static List<Peer> peers;
-	
 	public static void main(String[] args) {
-		String path = "src/GivenTools/newfile.txt";
+		String path = "C:\\Users\\chai1\\Desktop\\newfile.txt";
 		URL url = null;
 		String hostname = null;
 		int portno = -1;
-		HashMap tracker_info = null;
-		
 		//Open the torrent file and retrieve the TorrentInfo
 		TorrentInfo TI = openTorrent(args[0]);
 		url = getURL(TI);
@@ -48,14 +34,6 @@ public class RUBTClient {
 		System.out.println(hostname);
 		System.out.println(portno);
 		//Open connection...
-		
-		//connects to the tracker and retrieves the interval and peer list data
-		tracker_info = connectTracker(TI, url, hostname, portno);
-		interval = ((Integer)tracker_info.get("interval".getBytes())).intValue();
-		buildPeerList(tracker_info);
-		
-		
-		
 	}
 	
 	//Gets TorrentInfo from torrent file
@@ -89,81 +67,6 @@ public class RUBTClient {
 		return url;
 	}
 	
-	private static HashMap connectTracker(TorrentInfo TI, URL url, String hostname, int portno){
-		URL tracker = null;
-		Socket sock = null;
-		String getRequest = null;
-		HttpURLConnection tracker_connect = null;
-		HashMap decode = null;
-		
-		//opens socket to communicate with tracker
-		try{
-			sock = new Socket (hostname,portno);
-		}catch (Exception e){
-			System.out.println(e);
-		}
-		
-		//creating tracker connection url
-		try{
-			getRequest = url +
-					String.format("?info_hash=%s&peer_id=ABCDEFGHIJKLMNOPQRST&port=%s&uploaded=0&downloaded=0&left=%s", 
-					TI.info_hash,portno,TI.file_length);
-			tracker = new URL(getRequest);
-		}catch(Exception e){
-			System.out.println(e);
-		}
-		
-		//Making the connection with the tracker 
-		try {
-			tracker_connect = (HttpURLConnection)tracker.openConnection();
-		}catch (Exception e){ 
-			System.out.println(e);
-		}
-		
-		//try reading info from tracker
-		try{
-			BufferedInputStream tracker_response = new BufferedInputStream(tracker_connect.getInputStream());
-			ByteArrayOutputStream write_bytes = new ByteArrayOutputStream();
-			byte[] bytes = new byte[1];
-			
-			while(tracker_response.read(bytes) != -1)
-				write_bytes.write(bytes);
-			
-			byte[] response = write_bytes.toByteArray();
-			decode = (HashMap)Bencoder2.decode(response);	
-		}catch (Exception e){
-			System.out.println(e);
-		}
-		
-		//close socket
-		try{
-			sock.close();
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		
-		return decode;
-		
-	}
-	
-	private static void buildPeerList(HashMap info){
-		ArrayList list = (ArrayList)info.get("peers".getBytes());
-		
-		for(int i = 0; i < list.size(); i++){
-			HashMap peer_info = (HashMap)list.get(i);
-			
-			//gets peer id, ip and port
-			String peer_id = new String(((ByteBuffer)peer_info.get("peer id".getBytes())).array());
-			String ip = new String(((ByteBuffer)peer_info.get("ip".getBytes())).array());
-			int port = ((Integer)peer_info.get("port".getBytes())).intValue();
-			
-			//creates new peer and adds him to the peer list
-			//need to implement check according to assignment
-			// use only the peers at IP address with peer_id prefix RUBT11
-			Peer p = new Peer(peer_id, ip, port);
-			peers.add(p);
-		}
-	}
 	
 	private static void WriteToFile (String path) {
 		try {
@@ -172,11 +75,11 @@ public class RUBTClient {
 			byte[] bytes = hello.getBytes();
 			
 			FileOutputStream stream = new FileOutputStream(file);
-				try {
-				    stream.write(bytes);
-				} finally {
-				    stream.close();
-				}
+			try {
+			    stream.write(bytes);
+			} finally {
+			    stream.close();
+			}
 
 	    	} catch (IOException e) {
 		      e.printStackTrace();
