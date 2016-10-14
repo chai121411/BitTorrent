d7b60a4acd6c5a8afea6fcd5122a2f3f3911aef2
Terence Williams <richie2k693@yahoo.com>
2016-10-14 18:36:37 -0400

    PeerMessages
    Handles peer messages after handshake

---
+++ src/GivenTools/PeerMessages.java
@@ -0,0 +1,40 @@
+package GivenTools;
+
+public class PeerMessages {
+	
+	/**
+	 * Key for choke message
+	 */
+	final static int KEY_CHOKE = 0;
+	
+	/**
+	 * Key for unchoke message
+	 */
+	final static int KEY_UNCHOKE = 1;
+	
+	/**
+	 * Key for interested message
+	 */
+	final static int KEY_INTERESTED = 2;
+	
+	/**
+	 * Key for uninterested message
+	 */
+	final static int KEY_UNINTERESTED = 3;
+	
+	/**
+	 * Key for have message
+	 */
+	final static int KEY_HAVE = 4;
+
+	/**
+	 * Key for request message
+	 */
+	final static int KEY_REQUEST = 6;
+	
+	/**
+	 * Key for piece message
+	 */
+	final static int KEY_PIECE = 7;
+	
+}
