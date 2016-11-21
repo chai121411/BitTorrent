e2f0f1951a5e527d215e32e70b953817fe0ee296
min chai <jack@operationsrx.com>
2016-11-20 19:43:13 -0500

    phase iii start
    removed some old comments, added a TODO

--- src/GivenTools/Peer.java
+++ src/GivenTools/Peer.java
@@ -122,44 +122,8 @@ public class Peer {
 			p.start(this);
 			result = p.showInterest();
 			
-/**
- *  It is likely that at least the last block of the last piece will be smaller than the block request size 
- *  (and the last piece smaller than the piece_length) 
- *  because the total torrent length is unlikely to be evenly divisible by the piece size.
- *  You will need to take this into account when requesting the end of the torrent
- */
 			//unchoked and interested, can download file
 			if (result) {
-				/*Do a loop to request all the pieces...
-				**For the number of pieces needed to download the file??????? HOW MANY PIECES DO WE NEED???????????????????????????????????????????
-		
-					*Find piece length you need to download for the current piece index, apparently only the last piece has a different piece length
-					*Make a request to the peer with this piece index, piece length 
-					*(request: <len prefix> is 13 and msg ID is 6. The payload: <index><begin><length> 
-					
-					
-					* A peer should respond to a Request message with a ‘Piece’ message that includes the block requested. 
-					* Though the message type is called ‘Piece’, 
-					* it includes the information for a block, not necessarily a full piece.
-					* A Piece message consists of the 4-byte length prefix, 1-byte message ID, and a payload with a 4-byte piece index,
-					* 4-byte block offset within the piece in bytes (so far the same as for the Request message),
-					* and a variable length block containing the raw bytes for the requested piece.
-					* The length of this should be the same as the length requested.
-					*
-					
-					* When all blocks for a piece have been received, 
-					* you should perform a hash check to verify that the piece matches what is expected 
-					* and you have not been sent bad or malicious data.
-					* The ‘pieces’ element in the .torrent metafile includes a string of 20-byte hashes, one for each piece in the torrent. 
-					* Note that this is NOT a list, but is a single long string.
-					* You should perform a SHA1 hash on the downloaded piece contents 
-					* and compare that to the hash provided for that particular piece.
-					* If they do not match, you should discard the downloaded piece and request the blocks for it again.
-					***Looks like we need something in the torrent file which had a list of hashes i think...****
-				
-					*Write to peer that you "have" this piece. (verify the piece download with the piece_index just downloaded, PeerMessage.sendHave(piece_index)).
-			    	*have: <length prefix> is 5 and message ID is 4. The payload is a zero-based index of the piece that has just been downloaded and verified.
-				*/
 				
 				//The first time you begin the download,
 				//you need to contact the tracker and let it know you are starting to download.
@@ -178,24 +142,24 @@ public class Peer {
 					
 					keep_alive = System.nanoTime() - two_minutes;
 					
-					if(NANOSECONDS.toMinutes(keep_alive) >= 2){
+					if (NANOSECONDS.toMinutes(keep_alive) >= 2) {
 						p.keepAlive();
 						two_minutes = System.nanoTime();
 					}
 						
-					if(i+1 == piece_hashes.length){
+					if (i+1 == piece_hashes.length) {
 						
 						int temp = last_piece_length;
 						byte[] resultingPiece;
 						
-						while(temp > 0){
+						while (temp > 0) {
 							
-							if(temp > block_length){
+							if (temp > block_length) {
 								p.request(i, x, block_length);
 								resultingPiece = p.getPiece(block_length);
-							}else{
+							} else {
 								p.request(i, x, temp);
-								 resultingPiece= p.getPiece(temp);
+								resultingPiece= p.getPiece(temp);
 							}
 							
 							temp -= block_length;
@@ -203,10 +167,9 @@ public class Peer {
 							piece.write(resultingPiece);	
 						}
 						
-					}else{
-					
+					} else {
 						// gets all the blocks that make up a given piece
-						for(int j = 0; j < blocks_per_piece; j++){
+						for (int j = 0; j < blocks_per_piece; j++) {
 		 					p.request(i, x, block_length);
 		 					byte[] resultingPiece = p.getPiece(block_length);
 		 					x+= block_length;
@@ -219,28 +182,20 @@ public class Peer {
 					 *  has an SHA1 hash for each piece of the file and the pieces are verified as the finish downloading, 
 					 *  and are discarded if they fail to match the hash, indicating something wrong was transmitted to you.
 					 *  
-					 **  The MessageDigest class provides the functionality of a message digest algorithm, such as MD5 or SHA. 
-					 *  Message digests are secure one-way hash functions that take arbitrary-sized data and output a fixed-length hash value.
-					 *	
-					 * getInstance(String)
-					Generates a MessageDigest object that implements the specified digest algorithm.	
-					
-					 *  public byte[] digest(byte input[])
-					Performs a final update on the digest using the specified array of bytes, then completes the digest computation. That is, this method first calls update on the array, then calls digest().
-						Parameters:
-							input - the input to be updated before the digest is completed.
-						Returns:
-							the array of bytes for the resulting hash value.
-					
-					 * isEqual(byte[], byte[])
-					Compares two digests for equality.
-					
-					If not equal, resend request
+					 *  The MessageDigest class provides the functionality of a message digest algorithm, such as MD5 or SHA. 
+			
+					 *	If not equal, resend request
 					 */
 					byte[] SHA1digest = digestToSHA1(piece.toByteArray());
 					if (isEqualSHA1(piece_hashes[i].array(), SHA1digest)) {
-						if( (i+1) % 10 == 0)
+						if ((i+1) % 10 == 0) {
 							System.out.println("Piece " + (i+1) +" verified");
+						}
+						/**
+						 * If you wish to serve files as well as download them, 
+						 * you should send a Have message for the piece to all connected peers
+						 * once you have the full and hash-checked piece.
+						 */
 						p.sendHave(i);
 					} else {
 						System.out.println("Piece " + (i+1) +" IS NOT verified");
@@ -250,6 +205,7 @@ public class Peer {
 						continue;
 					}
 					
+					//TODO Store piece in a buffer, upload pieces to peers if requested, if we have the piece?
 					writeToFile(piece.toByteArray());
 				}
 				
@@ -411,3 +367,40 @@ public class Peer {
 		System.out.println("peerPort: " + getPeerPort());
 	}
 }
+
+
+/**
+ *  It is likely that at least the last block of the last piece will be smaller than the block request size 
+ *  (and the last piece smaller than the piece_length) 
+ *  because the total torrent length is unlikely to be evenly divisible by the piece size.
+ *  You will need to take this into account when requesting the end of the torrent
+ *  
+ *  *Do a loop to request all the pieces...
+				*For the number of pieces needed to download the file?
+		
+					*Find piece length you need to download for the current piece index, apparently only the last piece has a different piece length
+					*Make a request to the peer with this piece index, piece length 
+					*(request: <len prefix> is 13 and msg ID is 6. The payload: <index><begin><length> 
+					
+					* A peer should respond to a Request message with a ‘Piece’ message that includes the block requested. 
+					* Though the message type is called ‘Piece’, 
+					* it includes the information for a block, not necessarily a full piece.
+					* A Piece message consists of the 4-byte length prefix, 1-byte message ID, and a payload with a 4-byte piece index,
+					* 4-byte block offset within the piece in bytes (so far the same as for the Request message),
+					* and a variable length block containing the raw bytes for the requested piece.
+					* The length of this should be the same as the length requested.
+					*
+					
+					* When all blocks for a piece have been received, 
+					* you should perform a hash check to verify that the piece matches what is expected 
+					* and you have not been sent bad or malicious data.
+					* The ‘pieces’ element in the .torrent metafile includes a string of 20-byte hashes, one for each piece in the torrent. 
+					* Note that this is NOT a list, but is a single long string.
+					* You should perform a SHA1 hash on the downloaded piece contents 
+					* and compare that to the hash provided for that particular piece.
+					* If they do not match, you should discard the downloaded piece and request the blocks for it again.
+					***Looks like we need something in the torrent file which had a list of hashes i think...****
+				
+					*Write to peer that you "have" this piece. (verify the piece download with the piece_index just downloaded, PeerMessage.sendHave(piece_index)).
+			    	*have: <length prefix> is 5 and message ID is 4. The payload is a zero-based index of the piece that has just been downloaded and verified.
+ */
